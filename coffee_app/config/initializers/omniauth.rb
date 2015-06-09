139475a7aefbd896d98e192bcba411a8e50bc72b
kwokster10 <kwokster10@hotmail.com>
2015-06-08 22:45:29 -0400

    almost all working

--- coffee_app/config/initializers/omniauth.rb
+++ coffee_app/config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret
+  provider :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret, :display => "popup"
 end
 
 # , :scope => "email, name", :display => "popup"
