4eb4d6201de4357af2c634c4bcc91a186aabbe0c
Julie Kwok <jkwok@xogrp.com>
2017-01-22 00:22:35 -0500

    Updates secrets to use env variables

--- coffee_app/config/initializers/omniauth.rb
+++ coffee_app/config/initializers/omniauth.rb
@@ -3,4 +3,3 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.facebook_secret, :display => "popup"
 end
-
